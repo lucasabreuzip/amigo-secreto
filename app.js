56fc11fa01a4c9aaffb8b64705ef8f2f9cb6e0cf
lucasabreuzip <lucasabreu.abr@gmail.com>
2025-07-28 19:46:36 -0300

    Versão 1.1

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// FUNÇÃO PARA ADICIONAR AMIGO
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+
+    if (nomeAmigo == '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    if (amigos.includes(nomeAmigo)) {
+        alert('Nome já adicionado. Por favor, insira um nome diferente.');
+        document.getElementById('amigo').value = '';
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    atualizarLista();
+    document.getElementById('amigo').value = '';
+}
+
+// ATULIZA A LISTA DE AMIGOS
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+// SORTEIO DO AMIGO
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Adicione pelo menos 2 amigos para realizar o sorteio!');
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo secreto é: <strong>${amigoSorteado}</strong>`;
+}
+
+// FOI ADICIONADO O BOTÃO RESETAR
+function resetar() {
+    amigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+}
